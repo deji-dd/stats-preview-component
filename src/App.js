25433f522d04797ee9d3e8bb835886d9d8cd0cb3
deji-dd <dejib50@gmail.com>
2023-01-14 00:28:42 +0100

    Stats Preview Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import image from "./image.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-card row mx-auto">
+      <div className="col-6 col-text">
+        <div className="text-div mx-auto">
+          <div>
+            <h1 className="text-h1">
+              Get <span className="text-span">insights</span> that help your
+              business grow.
+            </h1>
+          </div>
+          <div className="text-div-p">
+            <p className="my-0">
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue customer experience and overall efficiency.
+            </p>
+          </div>
+          <div className="row stat-row">
+            <div className="col stat-div">
+              <h1 className="stat-heading">10k+</h1>
+              <h2 className="stat-p">COMPANIES</h2>
+            </div>
+            <div className="col stat-div">
+              <h1 className="stat-heading">314</h1>
+              <h2 className="stat-p">TEMPLATES</h2>
+            </div>
+            <div className="col stat-div">
+              <h1 className="stat-heading">12M+</h1>
+              <h2 className="stat-p">QUERIES</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-6 col-img img-div px-0">
+        <img src={image} className="img" alt="stuff"></img>
+        <span className="img-span"></span>
+      </div>
     </div>
   );
 }
